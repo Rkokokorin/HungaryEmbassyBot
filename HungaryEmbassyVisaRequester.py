24d0b62ee8ebddf7b22f3be27fed4222352c9789
Rkokokorin <roman@replan.city>
2025-02-07 18:40:57 +0100

    feat: option for belgrade return

--- HungaryEmbassyVisaRequester.py
+++ HungaryEmbassyVisaRequester.py
@@ -13,6 +13,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from tlg import send_telegram_update, SUCCESS_MESSAGE_TELEGRAM, FAIL_MESSAGE_TELEGRAM
 
+BELGRADE_XPATH = '//label[contains(text(), "Belgr")]'
+
+SUBOTICA_XPATH = '//label[contains(text(), "Subotica")]'
+
 RANDOM_MAX_MULTIPLIER = 1.3
 
 NIGHT_MODE_SLEEP_TIME = 1800
@@ -44,90 +48,8 @@ def check_exists_by_xpath(xpath):
         return False
     return True
 
-def belgrade (applicant_name, applicant_dob, applicant_count, applicant_phone, applicant_email, applicant_passport, applicant_country, applicant_residence):
-    
-    if driver.current_url == ("%s" % MFA_GOV_HU):
-         driver.close()
-         driver.get("%s" % MFA_GOV_HU)
-    else:
-         driver.get("%s" % MFA_GOV_HU)
-
-    locationElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id=\"label1\"]/button")))
-    action_chains.move_to_element(locationElement).click().perform()
-
-    input_location_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "/html/body/app/div/div/div[4]/div/div/div/div[2]/div[1]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/input")))
-    action_chains.move_to_element(input_location_element).click().send_keys("serbia").perform()
-    belgrade_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id=\"modal2\"]/div/div/div[2]/div[83]/label")))
-    action_chains.move_to_element(belgrade_element).click().perform()
-
-    visa_type_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//button[contains(@data-target, '#modalCases')]")))
-    action_chains.move_to_element(visa_type_element).click().perform()
-
-    input_visa_type_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "/html/body/app/div/div/div[4]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/div/div/div/div[2]/div[1]/input")))
-    action_chains.move_to_element(input_visa_type_element).click().send_keys("Visa").perform()
-    visa_c_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id=\"modalCases\"]/div/div/div[2]/div[40]/label")))
-    action_chains.move_to_element(visa_c_element).click().perform()
-
-    save_type_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id=\"modalCases\"]/div/div/div[3]/button[2]")))
-    action_chains.move_to_element(save_type_element).click().perform()
-
-    nameElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label4']")))
-    action_chains.move_to_element(nameElement).click().send_keys(applicant_name).perform()
-
-    birth_date_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='birthDate']")))
-    action_chains.move_to_element(birth_date_element).click().send_keys(applicant_dob).perform()
-
-    applicants_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label6']")))
-    driver.find_element(By.XPATH, "//*[@id='label6']").clear()
-    action_chains.move_to_element(applicants_element).click().send_keys(applicant_count).perform()
-
-    phone_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label9']")))
-    action_chains.move_to_element(phone_element).click().send_keys(applicant_phone).perform()
-
-    email_element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label10']")))
-    action_chains.move_to_element(email_element).click().send_keys(applicant_email).perform()
-
-    passportNumberElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label1000']")))
-    action_chains.move_to_element(passportNumberElement).click().send_keys(applicant_passport).perform()
-
-    countryElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label1001']")))
-    action_chains.move_to_element(countryElement).click().send_keys(applicant_country).perform()
-
-    residenceElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label1002']")))
-    action_chains.move_to_element(residenceElement).click().send_keys(applicant_residence).perform()
-
-    checkbox1Element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='slabel13']")))
-    action_chains.move_to_element(checkbox1Element).click().perform()
-
-    checkbox2Element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id='label13']")))
-    action_chains.move_to_element(checkbox2Element).click().perform()
-
-    saveElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-primary w-100']")))
-    action_chains.move_to_element(saveElement).click().perform()
-
-    errorElement = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, "//*[@id=\"Torles\"]/div/div/div[1]/div")))
-
-def subotica(applicant_name, applicant_dob, applicant_count, applicant_phone, applicant_email, applicant_passport, applicant_country,
-             applicant_residence_permit):
+def run(applicant_name, applicant_dob, applicant_count, applicant_phone, applicant_email, applicant_passport, applicant_country,
+        applicant_residence_permit, is_belgrade):
     try:
         if driver.current_url == "https://konzinfobooking.mfa.gov.hu":
              driver.close()
@@ -141,8 +63,13 @@ def subotica(applicant_name, applicant_dob, applicant_count, applicant_phone, ap
         input_location_element = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH, "/html/body/app/div/div/div[4]/div/div/div/div[2]/div[1]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/input")))
         action_chains.move_to_element(input_location_element).click().send_keys("serbia").perform()
+        subotica_ = SUBOTICA_XPATH
+        xpath = subotica_
+        belgrade_ = BELGRADE_XPATH
+        if is_belgrade:
+            xpath = belgrade_
         label = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//label[contains(text(), "Subotica")]')) )
+            EC.presence_of_element_located((By.XPATH, xpath)) )
         parent = label.find_element(By.XPATH, './..')
         checkbox = parent.find_element(By.XPATH, './/input[@class="form-check-input"]')
         action_chains.move_to_element(checkbox).click().perform()
@@ -235,51 +162,46 @@ def subotica(applicant_name, applicant_dob, applicant_count, applicant_phone, ap
         sys.exit(-1)
 
 
-def startbg():
-    belgrade(ent_name.get(), ent_dob.get(), ent_count.get(), ent_phone.get(), ent_email.get(), ent_passport.get(), ent_country.get(), ent_residence.get())
-    time.sleep(5)
-    if check_exists_by_xpath("//*[@id=\"Torles\"]/div/div/div[2]/button") == True:
-            time.sleep(random.randint(80, 100))
-            startbg()
-    else:
-        time.sleep(1800)
-
 def startsb():
     option.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36")
     option.add_argument("Accept-Language=en-US,en;q=0.9")
     print("run: " + time.ctime())
+    i = SLEEP_TIME_BETWEEN_REQUESTS
+    if varSub.get():
+        is_belgrade = False
+        run_check_city(is_belgrade)
+    if varBeog.get():
+        if varSub.get():
+            i = int(i/2)
+            time.sleep(random.randint(i, int(i * RANDOM_MAX_MULTIPLIER)))
+        is_belgrade = True
+        run_check_city(is_belgrade)
+    if 1 <= time.localtime().tm_hour <= 6:
+        print("Night mode")
+        i = NIGHT_MODE_SLEEP_TIME
+    time.sleep(random.randint(i, int(i * RANDOM_MAX_MULTIPLIER)))
+    startsb()
+
+
+def run_check_city(is_belgrade):
     if var.get():
         json_data = read_json(ent_path.get())
-        subotica(json_data.get("name"), json_data.get("birth_date"), json_data.get("count"),
-                 json_data.get("phone"), json_data.get("email"), json_data.get("passport"),"Russian Federation",json_data.get("residence_permit"))
+        run(json_data.get("name"), json_data.get("birth_date"), json_data.get("count"),
+            json_data.get("phone"), json_data.get("email"), json_data.get("passport"), "Russian Federation",
+            json_data.get("residence_permit"), is_belgrade)
     else:
-        subotica(ent_name.get(), ent_dob.get(), ent_count.get(), ent_phone.get(), ent_email.get(), ent_passport.get(), ent_country.get(), ent_residence)
+        run(ent_name.get(), ent_dob.get(), ent_count.get(), ent_phone.get(), ent_email.get(), ent_passport.get(),
+            ent_country.get(), ent_residence, is_belgrade)
     time.sleep(5)
-    if check_exists_by_xpath("//*[@id=\"Torles\"]/div/div/div[2]/button") and check_exists_by_xpath("//div[contains(text(), \"no appointments available\")]"):
-            i = SLEEP_TIME_BETWEEN_REQUESTS
-            if 1 <= time.localtime().tm_hour <= 6:
-                print("Night mode")
-                i = NIGHT_MODE_SLEEP_TIME
-            time.sleep(random.randint(i, int(i * RANDOM_MAX_MULTIPLIER)))
-            startsb()
+    if check_exists_by_xpath("//*[@id=\"Torles\"]/div/div/div[2]/button") and check_exists_by_xpath(
+            "//div[contains(text(), \"no appointments available\")]"):
+        print("fail")
     else:
         send_telegram_update(SUCCESS_MESSAGE_TELEGRAM)
         time.sleep(15)
         send_telegram_update(SUCCESS_MESSAGE_TELEGRAM)
         time.sleep(15000)
 
-def startbgsb():
-    belgrade(ent_name.get(), ent_dob.get(), ent_count.get(), ent_phone.get(), ent_email.get(), ent_passport.get(), ent_country.get(), ent_residence.get())
-    if check_exists_by_xpath("//*[@id=\"Torles\"]/div/div/div[2]/button") == True:
-            time.sleep(random.randint(80, 100))
-            subotica(ent_name.get(), ent_dob.get(), ent_count.get(), ent_phone.get(), ent_email.get(), ent_passport.get(), "Russian Federation", ent_residence.get())
-            if check_exists_by_xpath("//*[@id=\"Torles\"]/div/div/div[2]/button") == True:
-                time.sleep(random.randint(80, 100))
-                startbgsb()
-            else:
-                time.sleep(SLEEP_SECONDS)
-    else:
-        time.sleep(SLEEP_SECONDS)
 
 def emulate_human():
     time.sleep(1)
@@ -336,28 +258,31 @@ lbl_path.grid(row=8, column=0, sticky="e")
 ent_path.grid(row=8, column=1, sticky="w")
 
 var = tk.BooleanVar(value=False)
+varSub = tk.BooleanVar(value=False)
+varBeog = tk.BooleanVar(value=False)
 
 chk_read_json = tk.Checkbutton(
     master=window,
     text="Read json",
     variable=var
 )
-
-btn_belgrade = tk.Button(
+chk_bg = tk.Checkbutton(
     master=window,
-    text="Start Belgrade",
-    command=startbg
+    text="Beograd",
+    variable=varBeog
 )
+chk_sub = tk.Checkbutton(
+    master=window,
+    text="Subotica",
+    variable=varSub
+)
+
 btn_subotica = tk.Button(
     master=window,
-    text="Start Subotica",
+    text="Start",
     command=startsb
 )
-btn_belgrade_subotica = tk.Button(
-    master=window,
-    text="Start Belgrade and Subotica",
-    command=startbgsb
-)
+
 btn_stop = tk.Button(
     master=window,
     text="Exit",
@@ -367,6 +292,8 @@ btn_stop = tk.Button(
 frmEntry.grid(row=0, column=0, padx=10)
 btn_subotica.grid(row=0, column=2, pady=10)
 chk_read_json.grid(row=1, column=4, pady=10)
+chk_bg.grid(row=1, column=1, pady=10)
+chk_sub.grid(row=1, column=2, pady=10)
 btn_stop.grid(row=0, column=4, padx=10)
 
 window.mainloop()
